d4a83bf579b9b23ef78721bc527c9c8c0e6a5381
Jarak-Jakar <15709236+Jarak-Jakar@users.noreply.github.com>
2024-05-19 11:36:21 +1200

    Wrap server bits in a try-catch-finally block

--- src/Server.cs
+++ src/Server.cs
@@ -2,8 +2,27 @@ using System.Net;
 using System.Net.Sockets;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+// Console.WriteLine("Logs from your program will appear here!");
 
-var server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-server.AcceptSocket(); // wait for client
+const int portNumber = 4221;
+TcpListener server = null;
+
+try
+{
+    server = new TcpListener(IPAddress.Any, portNumber);
+    server.Start();
+
+    while (true)
+    {
+        Console.WriteLine("Awaiting a connection");
+        server.AcceptSocket(); // wait for client
+    }
+}
+catch (SocketException exception)
+{
+    Console.Error.WriteLine($"SocketException: {exception}");
+}
+finally
+{
+    server?.Stop();
+}
